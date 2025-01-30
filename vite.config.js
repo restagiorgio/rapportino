e9581a98a6cf3e4570efa8fead3130876e46ac4d
restagiorgio <giorgio_resta@outlook.com>
2025-01-30 17:28:29 +0100

    fix: corretta configurazione per github pages

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  base: './',  // Cambiato da '/rapportino/' a './'
+  base: '/rapportino/',
   plugins: [react()],
   build: {
-    outDir: 'dist',
-    assetsDir: 'assets',
-    rollupOptions: {
-      output: {
-        assetFileNames: 'assets/[name].[ext]'
-      }
-    }
+    outDir: 'dist'
   }
 })
